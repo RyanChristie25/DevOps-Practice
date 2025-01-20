616dc69cf6da3c8078acefe2f84318171924923e
Ryan Christie <ryan.christie@bbd.co.za>
2025-01-20 20:06:54 +0200

    Working index.js code

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.listen(port, () => {
-    console.log("The server is running on port 3000");
-});
-
-app.get("/clubs", (req, res, next) => {
-    res.json(["Soccer","Padel","Chess","Gaming","Hiking"]);
-});
+exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Hello, World!",
+        }),
+    };
+};
